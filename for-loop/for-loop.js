b77cebdbeb5ba29426c80de569154ea74aff3394
Yurika Hokama <yurihoka@gmail.com>
2023-11-15 12:19:34 -0800

    practice for for-loop

---
+++ for-loop/for-loop.js
@@ -0,0 +1,11 @@
+const orders = [
+  { product: "burger", quantity: 2 },
+  { product: "fries", quantity: 1 },
+  { product: "shake", quantity: 1 },
+  { product: "soda", quantity: 1 },
+];
+
+
+for (const order of orders) {
+    console.log(order);
+}
